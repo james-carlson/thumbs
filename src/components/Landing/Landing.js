eab5104b0c356993ed775be85b4ecb5c94cdce11
James Carlson <carlson.james@gmail.com>
2017-11-11 13:26:32 -0700

    improved landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -25,28 +25,34 @@ class Landing extends Component {
     render() {
         return (
             <div>
-                {/* {listenForSuccessfulSocketConnection('instructor', this.props.class_sessionID)} */}
                 <Link to={`/${this.props.class_sessionID}`} onClick={this.handleInitializeUser()}>
-                    <div className="landing_header">
-                        <div className="landing_logo">
-                            <div className="landing_overlay">
-                                <div className="landing_appName">THUMBS</div>
-                                <div className="tagline">Instant,</div>
-                                <div className="tagline">Accurate</div>
-                                <div className="tagline">Feedback</div>
+                        <div className="landing_header">
+                            <div className="landing_logo">
+                                <div className="landing_overlay">
+                                    <div className="landing_appName">THUMBS</div>
+                                    <div className="tagline">Instant,</div>
+                                    <div className="tagline">Accurate</div>
+                                    <div className="tagline">Feedback</div>
+                                </div>
+                            </div>
+                            <div className="landing_instructions top_padding">To get started, click.</div>
+                        </div>
+                        <div className="landing_main">
+                            <div className="welcome_content">
+                                {/* <h1 className="welcome_header">Welcome to Thumbs!</h1> */}
+                                <p><b>What is Thumbs?</b><br />
+                                    Thumbs lets you know what students (or others) are <i>really</i> thinking, in real-time.</p>
+                                <p><b>How does it work?</b><br />
+                                    Click anywhere on this page to get started.
+                            Fill out the information about your session of Thumbs.
+                            Then click "Go Live", send the URL out (you can click on the clipboard to grab the URL),
+                            and anyone visiting your link will be able to see your session.
+                            You'll be able to send questions and see responses in real-time.</p>
+                                <p><b>Note</b><br />
+                                    The initial version of Thumbs was personalized for DevMountain.
+                            If you would like to use Thumbs in your organization, <a href="https://www.linkedin.com/in/james-d-carlson/">get in touch</a>!</p>
                             </div>
                         </div>
-                        <div className="landing_instructions top_padding">To get started, click anywhere.</div>
-                    </div>
-                <div className="landing_main">
-                    <div className="welcome_content">
-                    <h1 className="welcome_header">Welcome to Thumbs.</h1>
-                        <p><b>What is Thumbs?</b><br />
-                            Thumbs lets you know what students (or others) are <i>really</i> thinking, in real-time.</p>
-                        <p><b>How does it work?</b><br />
-                            Click on the logo above, and fill out the information about your session of Thumbs. Then click "Go Live", send the URL out, and people will be able to connect to your session. You'll be able to send questions and see responses in real-time.</p>
-                    </div>
-                </div>
                 </Link>
             </div>
 
